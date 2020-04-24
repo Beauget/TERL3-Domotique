63932d6bb91a944b37bf473830f77afd1b902845
Appixroid <appixroid@gmail.com>
2020-04-24 14:08:43 +0200

    Adding button for disabling auto heater

--- code/Heater/Heater.lua
+++ code/Heater/Heater.lua
@@ -4,9 +4,10 @@ commandArray = {}
 
 temperatureDevice = 'Température / Humidité'; -- Temperature Sensor
 targetTemperatureDevice = 'Température Idéale'; -- Target Temperature Selector
+autoSwitch = 'Chauffage Auto.'; -- Switch to enable auto heater
 heaterDevice = 'Chauffage'; -- Heater Controler
 
-if devicechanged[temperatureDevice] then
+if devicechanged[temperatureDevice] and otherdevices[autoSwitch] == 'On' then
     if tonumber(otherdevices_temperature[temperatureDevice]) < tonumber(otherdevices[targetTemperatureDevice]) then
         commandArray[heaterDevice] = 'On'
     else
